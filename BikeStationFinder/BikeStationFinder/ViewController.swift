306d7a8f9281bea25aba617b38d80dd8f91dd2db
rsgc-McDaniel-H <hmcdaniel@rsgc.on.ca>
2016-05-19 12:19:58 -0400

    Added a view to the controller

---
+++ BikeStationFinder/BikeStationFinder/ViewController.swift
@@ -0,0 +1,225 @@
+//
+//  ViewController.swift
+//  BikeStationFinder
+//
+//  Created by Student on 2016-05-18.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController : UIViewController {
+    
+    // Views that need to be accessible to all methods
+    let jsonResult = UILabel()
+    
+    // If data is successfully retrieved from the server, we can parse it here
+    func parseMyJSON(theData : NSData) {
+        
+        
+        // De-serializing JSON can throw errors, so should be inside a do-catch structure
+        do {
+            
+            // Do the initial de-serialization
+            // Source JSON is here:
+            // http://feeds.bikesharetoronto.com/stations/stations.json
+            //
+            let json = try NSJSONSerialization.JSONObjectWithData(theData, options: NSJSONReadingOptions.AllowFragments)as! AnyObject
+            
+            if let stationData = json as? [String: AnyObject] {
+                print("stationData is: \(stationData["executionTime"])")
+            } else {
+                print("could not parse station data")
+            }
+            
+            if let beanList = json as? AnyObject {
+                //print("beanList is: \(beanList["stationBeanList"])")
+                
+                if let beans = beanList["stationBeanList"] as? [AnyObject] {
+                    
+                    for bean in beans {
+                        
+                        if let stationDetails = bean as? [String: AnyObject]{
+                            
+                            //print("station name is \(stationDetails["stationName"])")
+                            
+                            guard let name : String = stationDetails["stationName"] as? String,
+                                let latitude : Double = stationDetails["latitude"] as? Double,
+                                let longitude : Double = stationDetails["longitude"] as? Double
+                                else {
+                                    print("Error loading")
+                                    return
+                            }
+                            
+                            print("stationName is \(name)")
+                            print("latitude is \(latitude)")
+                            print("longitude is \(longitude)")
+                            
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self.jsonResult.text = "The Station closest to you is \(name)"
+                            }
+                        }
+                
+                    }
+                    
+                }
+                
+                
+            } else {
+                print("error retreving bean list ")
+            }
+            
+            // Now we can update the UI
+            // (must be done asynchronously)
+           
+            
+        } catch let error as NSError {
+            print ("Failed to load: \(error.localizedDescription)")
+        }
+        
+        
+    }
+    
+    // Set up and begin an asynchronous request for JSON data
+    func getMyJSON() {
+        
+        // Define a completion handler
+        // The completion handler is what gets called when this **asynchronous** network request is completed.
+        // This is where we'd process the JSON retrieved
+        let myCompletionHandler : (NSData?, NSURLResponse?, NSError?) -> Void = {
+            
+            (data, response, error) in
+            
+            // This is the code run when the network request completes
+            // When the request completes:
+            //
+            // data - contains the data from the request
+            // response - contains the HTTP response code(s)
+            // error - contains any error messages, if applicable
+            
+            // Cast the NSURLResponse object into an NSHTTPURLResponse objecct
+            if let r = response as? NSHTTPURLResponse {
+                
+                // If the request was successful, parse the given data
+                if r.statusCode == 200 {
+                    
+                    if let d = data {
+                        
+                        // Parse the retrieved data
+                        self.parseMyJSON(d)
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        // Define a URL to retrieve a JSON file from
+        let address : String = "http://feeds.bikesharetoronto.com/stations/stations.json"
+        
+        // Try to make a URL request object
+        if let url = NSURL(string: address) {
+            
+            // We have an valid URL to work with
+            print(url)
+            
+            // Now we create a URL request object
+            let urlRequest = NSURLRequest(URL: url)
+            
+            // Now we need to create an NSURLSession object to send the request to the server
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            // Now we create the data task and specify the completion handler
+            let task = session.dataTaskWithRequest(urlRequest, completionHandler: myCompletionHandler)
+            
+            // Finally, we tell the task to start (despite the fact that the method is named "resume")
+            task.resume()
+            
+        } else {
+            
+            // The NSURL object could not be created
+            print("Error: Cannot create the NSURL object.")
+            
+        }
+        
+    }
+    
+    // This is the method that will run as soon as the view controller is created
+    override func viewDidLoad() {
+        
+        // Sub-classes of UIViewController must invoke the superclass method viewDidLoad in their
+        // own version of viewDidLoad()
+        super.viewDidLoad()
+        
+        // Make the view's background be gray
+        view.backgroundColor = UIColor.lightGrayColor()
+        
+        /*
+         * Further define label that will show JSON data
+         */
+        
+        // Set the label text and appearance
+        jsonResult.text = "Information will load here"
+        jsonResult.font = UIFont.systemFontOfSize(12)
+        jsonResult.numberOfLines = 0   // makes number of lines dynamic
+        // e.g.: multiple lines will show up
+        jsonResult.textAlignment = NSTextAlignment.Center
+        
+        // Required to autolayout this label
+        jsonResult.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the label to the superview
+        view.addSubview(jsonResult)
+        
+        /*
+         * Add a button
+         */
+        let getData = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 30))
+        
+        // Make the button, when touched, run the calculate method
+        getData.addTarget(self, action: #selector(ViewController.getMyJSON), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // Set the button's title
+        getData.setTitle("Get my JSON!", forState: UIControlState.Normal)
+        
+        // Required to auto layout this button
+        getData.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the button into the super view
+        view.addSubview(getData)
+        
+        /*
+         * Layout all the interface elements
+         */
+        
+        // This is required to lay out the interface elements
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Create an empty list of constraints
+        var allConstraints = [NSLayoutConstraint]()
+        
+        // Create a dictionary of views that will be used in the layout constraints defined below
+        let viewsDictionary : [String : AnyObject] = [
+            "title": jsonResult,
+            "getData": getData]
+        
+        // Define the vertical constraints
+        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-50-[getData]-[title]",
+            options: [],
+            metrics: nil,
+            views: viewsDictionary)
+        
+        // Add the vertical constraints to the list of constraints
+        allConstraints += verticalConstraints
+        
+        // Activate all defined constraints
+        NSLayoutConstraint.activateConstraints(allConstraints)
+        
+    }
+    
+}
+
---
+++ BikeStationFinder/BikeStationFinderTests/BikeStationFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  BikeStationFinderTests.swift
+//  BikeStationFinderTests
+//
+//  Created by Student on 2016-05-18.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import XCTest
+@testable import BikeStationFinder
+
+class BikeStationFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
